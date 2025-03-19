72320a4d6ca2f5444bd741d8fb783eabef5a457c
Dhanushvarma <pdhanu2001@gmail.com>
2025-03-19 15:49:01 -0700

    feature : made it work with MujocoEnv (from Gymnasium Repo)

--- ss/envs/franka_env.py
+++ ss/envs/franka_env.py
@@ -1,62 +1,36 @@
-import numpy as np
-import time
 import os
 
-import gymnasium as gym
-from gymnasium import spaces
+import numpy as np
 import mujoco
-import mujoco.viewer
-import mediapy as media
+from gymnasium.envs.mujoco import MujocoEnv
+from gymnasium import spaces
+from gymnasium.envs.mujoco.mujoco_rendering import OffScreenViewer
 
 
-class FrankaEnv(gym.Env):
-    """Gym environment for Franka Panda arm with operational space control."""
+class FrankaEnv(MujocoEnv):
+    def __init__(self, model_path=None, render_mode=None):
+        observation_space = spaces.Box(
+            low=-np.inf, high=np.inf, shape=(21,), dtype=np.float32
+        )
 
-    metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 100}
-
-    def __init__(self, render_mode=None, xml_path=None):
-        """Initialize environment with operational space controller.
-
-        Args:
-            render_mode: The render mode to use, either "human" or "rgb_array"
-            xml_path: Path to the MuJoCo XML file for the Franka scene
-        """
-        # Default XML path
-        if xml_path is None:
+        # default model path
+        if model_path is None:
             pwd = os.path.dirname(os.path.abspath(__file__))
-            xml_path = os.path.join(pwd, "../../assets/franka_emika_panda/scene.xml")
+            model_path = os.path.join(pwd, "../../assets/franka_emika_panda/scene.xml")
 
-        # Load model and data
-        self.model = mujoco.MjModel.from_xml_path(xml_path)
-        self.data = mujoco.MjData(self.model)
+        super().__init__(
+            model_path=model_path,
+            frame_skip=1,
+            observation_space=observation_space,
+            render_mode=render_mode,
+            width=480,
+            height=480,
+        )
 
-        # Simulation settings
-        self.dt = 0.001
-        self.model.opt.timestep = self.dt
+        self._init_osc_params()
+        self._init_other_renders()
 
-        # Controller constants
-        self.home_joint_pos = [
-            0,
-            0,
-            0,
-            -1.57079,
-            0,
-            1.57079,
-            -2.29,
-        ]  # except the hand joints
-        self.impedance_pos = np.array([500.0, 500.0, 500.0])  # [N/m]
-        self.impedance_ori = np.array([250.0, 250.0, 250.0])  # [Nm/rad]
-        self.Kp_null = np.array([10.0] * 7)  # NOTE: got it from isaacgymenvs
-        self.damping_ratio = 1.0
-        self.Kpos = 0.9
-        self.Kori = 0.9
-        self.integration_dt = 1.0
-        self.gravity_compensation = True
-
-        # Get important model IDs
-        self.site_name = "attachment_site"
-        self.site_id = self.model.site(self.site_name).id
-
+        # Franks ID's
         self.joint_names = [
             "joint1",
             "joint2",
@@ -72,11 +46,52 @@ class FrankaEnv(gym.Env):
         self.actuator_ids = np.array(
             [self.model.actuator(name).id for name in self.joint_names]
         )
+
+        # Attachment Site
+        self.site_name = "attachment_site"
+        self.site_id = self.model.site(self.site_name).id
+
+        # Franks actuator & joints
         self.grip_actuator_id = self.model.actuator("hand").id
         self.robot_n_dofs = len(self.joint_names)
 
+        # mocap, used as target
         self.mocap_name = "target"
         self.mocap_id = self.model.body(self.mocap_name).mocapid[0]
+
+        # reset joint position for the Franka Arm
+        self.home_joint_pos = [
+            0,
+            0,
+            0,
+            -1.57079,
+            0,
+            1.57079,
+            -2.29,
+        ]  # except the hand joints
+
+        # action Space
+        self.action_space = spaces.Tuple(
+            (
+                spaces.Box(low=-0.1, high=0.1, shape=(6,), dtype=np.float32),
+                spaces.Discrete(2),
+            )
+        )
+
+    def _init_osc_params(self):
+        """
+        All the OSC related parameters
+        """
+
+        # osc parameters
+        self.impedance_pos = np.array([500.0, 500.0, 500.0])  # [N/m]
+        self.impedance_ori = np.array([250.0, 250.0, 250.0])  # [Nm/rad]
+        self.Kp_null = np.array([10.0] * 7)  # NOTE: got it from isaacgymenvs
+        self.damping_ratio = 1.0
+        self.Kpos = 0.9
+        self.Kori = 0.9
+        self.integration_dt = 1.0
+        self.gravity_compensation = True
 
         # Controller matrices
         damping_pos = self.damping_ratio * 2 * np.sqrt(self.impedance_pos)
@@ -97,61 +112,60 @@ class FrankaEnv(gym.Env):
         # workspace bounds
         self._workspace_bounds = np.array([[0.15, 0.615], [-0.35, 0.35], [0, 0.6]])
 
-        # Define action spaces as specified
-        continuous_space = spaces.Box(low=-1, high=1, shape=(6,), dtype=np.float64)
-        discrete_space = spaces.Discrete(2)  # 0 = open, 1 = close
-        self.action_space = spaces.Tuple((continuous_space, discrete_space))
+    def _init_other_renders(self):
 
-        # default obs: joint positions (7), joint velocities (7), end effector position (3), end effector orientation (4)
-        self.observation_space = spaces.Box(
-            low=-np.inf, high=np.inf, shape=(21,), dtype=np.float64
+        self.offscreen_renderer = OffScreenViewer(
+            model=self.model, data=self.data, width=224, height=224
         )
 
-        # Set up rendering
-        self.render_mode = render_mode
-        self.viewer = None
-        self.step_start = None
-
-    def reset(self, seed=None, options=None):
-        """Reset the environment to initial state."""
-        super().reset(seed=seed)
-
-        # Reset data
-        mujoco.mj_resetData(self.model, self.data)
+    def reset_model(self):
         self.data.qpos[self.dof_ids] = self.home_joint_pos
-
-        # Do one step to update derived quantities
         mujoco.mj_forward(self.model, self.data)
+        return self._get_obs()
 
-        # Initialize target at current end effector position
-        self.data.mocap_pos[self.mocap_id] = self.data.site(self.site_id).xpos.copy()
-        self.data.mocap_quat[self.mocap_id] = np.array([1, 0, 0, 0])
+    def _get_obs(self):
+        joint_pos = self.data.qpos[self.dof_ids]
+        joint_vel = self.data.qvel[self.dof_ids]
+        ee_pos = self.data.site(self.site_id).xpos
+        ee_quat = np.zeros(4)
+        mujoco.mju_mat2Quat(ee_quat, self.data.site(self.site_id).xmat)
+
+        # below code works, but acts weird.
         if False:
-            mujoco.mju_mat2Quat(
-                self.data.mocap_quat[self.mocap_id], self.data.site(self.site_id).xmat
+            _camera_id = self.model.camera("topview").id
+            rgb = self.offscreen_renderer.render(
+                render_mode="rgb_array", camera_id=_camera_id
             )
 
-        # Get observation
-        observation = self._get_obs()
-        info = self._get_info()
-
-        if self.render_mode == "human":
-            self._render_frame()
-
-        return observation, info
+        return np.concatenate([joint_pos, joint_vel, ee_pos, ee_quat])
 
     def step(self, action):
-
-        # unpack
         continuous_action, grip = action
+        robot_tau = self._compute_osc(continuous_action)
+        ctrl = np.zeros(self.model.nu)
+        ctrl[self.actuator_ids] = robot_tau
+        ctrl[self.grip_actuator_id] = 255 if grip == 0 else 0
+        self.do_simulation(ctrl, self.frame_skip)
+
+        obs = self._get_obs()
+        reward = 0.0  # Customize as needed
+        terminated = False
+        truncated = False
+        info = {}
+
+        if self.render_mode == "human":
+            self.render()
+
+        return obs, reward, terminated, truncated, info
+
+    def _compute_osc(self, continuous_action):
 
         # current mocap
         current_pos = self.data.mocap_pos[self.mocap_id]
         current_quat = self.data.mocap_quat[self.mocap_id]
-        print("Current Quat", current_quat)
 
         # delta translation
-        pos_scale = 1  # 5cm maximum movement per step
+        pos_scale = 1
         pos_delta = continuous_action[:3] * pos_scale
         new_pos = current_pos + pos_delta
         new_pos = np.clip(
@@ -167,7 +181,7 @@ class FrankaEnv(gym.Env):
         self.data.mocap_pos[self.mocap_id] = new_pos
         self.data.mocap_quat[self.mocap_id] = current_quat
 
-        # OSC Logic Begins #
+        ### NOTE : OSC Logic Begins ###
 
         # 1. Compute twist
         dx = self.data.mocap_pos[self.mocap_id] - self.data.site(self.site_id).xpos
@@ -221,82 +235,13 @@ class FrankaEnv(gym.Env):
         if self.gravity_compensation:
             robot_tau += self.data.qfrc_bias[self.dof_ids]
 
-        # 7. Apply control and step
+        # 7. Clip to actuator limits
         np.clip(
             robot_tau,
             *self.model.actuator_ctrlrange[: self.robot_n_dofs, :].T,
             out=robot_tau,
         )
-        self.data.ctrl[self.actuator_ids] = robot_tau
 
-        # OSC Login Ends #
+        ### NOTE : OSC Logic Ends ###
 
-        # Actuate the gripper, tendon for franka
-        self.data.ctrl[self.grip_actuator_id] = 255 if grip == 0 else 0
-
-        # Step the simulation
-        mujoco.mj_step(self.model, self.data)
-
-        # Get observation and info
-        observation = self._get_obs()
-        reward = 0.0  # Placeholder - you'll implement this
-        terminated = False  # Placeholder - you'll implement this
-        truncated = False  # Placeholder - you'll implement this
-        info = {}  # Placeholder - you'll implement this
-
-        if self.render_mode == "human":
-            self._render_frame()
-
-        return observation, reward, terminated, truncated, info
-
-    def render(self):
-        """Render the environment."""
-        if self.render_mode == "rgb_array":
-            return self._render_frame()
-
-    def _render_frame(self):
-        """Render a single frame."""
-        if self.viewer is None and self.render_mode == "human":
-            self.viewer = mujoco.viewer.launch_passive(
-                model=self.model,
-                data=self.data,
-                show_left_ui=False,
-                show_right_ui=False,
-            )
-
-        if self.step_start is None and self.render_mode == "human":
-            self.step_start = time.time()
-
-        if self.render_mode == "human":
-            self.viewer.sync()
-            time_until_next_step = self.dt - (time.time() - self.step_start)
-            if time_until_next_step > 0:
-                time.sleep(time_until_next_step)
-            self.step_start = time.time()
-
-        elif self.render_mode == "rgb_array":
-            # Create renderer for generating an image
-            width, height = 640, 480
-            renderer = mujoco.Renderer(self.model, width, height)
-            renderer.update_scene(self.data)
-            return renderer.render()
-
-    def close(self):
-        """Close the environment."""
-        if self.viewer is not None:
-            self.viewer.close()
-            self.viewer = None
-
-    def _get_obs(self):
-        # TODO: Implement this in subclass
-
-        with mujoco.Renderer(self.model) as renderer:
-            renderer.update_scene(self.data, camera="topview")
-            image = renderer.render()
-            print(image.shape)
-
-        return np.zeros(self.observation_space.shape[0])
-
-    def _get_info(self):
-        # TODO: Implement this in subclass
-        return {}
+        return robot_tau
